7c61a8adfdc97c064bfc65fc2d0ec3480c68391e
Henderb Rodriguez <henderbj@gmail.com>
2016-11-19 23:21:28 +0100

    Configure save passwords as md5 on mysql. Configure freeradius for MYSQL.

--- freeradius-v2/install.sh
+++ freeradius-v2/install.sh
@@ -31,3 +31,18 @@ sed -ibak -e "s/\$configValues\['CONFIG_DB_USER'\] = 'root';/\
 
 sed -ibak -e "s/\$configValues\['CONFIG_DB_PASS'\] = '';/\
 \$configValues\['CONFIG_DB_PASS'\] = '$SQLPASS';/" $daloconf
+
+# Configure save passwords as md5 on mysql
+sed -ibak -e "s/\$configValues\['CONFIG_DB_PASSWORD_ENCRYPTION'\] =.*;/\
+\$configValues\['CONFIG_DB_PASSWORD_ENCRYPTION'\] = 'md5';/" $daloconf
+
+# Configure freeradius for MYSQL
+sed -i".bak" -e "s/\#.*\$INCLUDE sql.conf/        \$INCLUDE sql.conf/" /etc/freeradius/radiusd.conf
+file=/etc/freeradius/sql.conf
+sed -i".bak" -e "s/server = \"localhost\"/server = \"$SQLHOST\"/" $file
+sed -i".bak" -e "s/login = \"radius\"/login = \"$SQLUSER\"/" $file
+sed -i".bak" -e "s/password = \"radpass\"/password = \"$SQLPASS\"/" $file
+sed -i".bak" -e "s/#readclients = yes/readclients = yes/" $file
+file=/etc/freeradius/sites-enabled/default
+sed -i".bak" -e "s/#\ssql$/\tsql/" $file
+sed -i".bak" -e "s/#\sreply_log$/\treply_log/" $file
